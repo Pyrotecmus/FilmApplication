490c22026ee3e41e4bcec1336c497a57e1f5003d
Axel Imberdis <imberdis.axel@gmail.com>
2019-02-26 19:03:29 +0100

    other folder

---
+++ FilmListTests/FilmListTests.swift
@@ -0,0 +1,34 @@
+//
+//  FilmListTests.swift
+//  FilmListTests
+//
+//  Created by Axel Imberdis on 21/02/2019.
+//  Copyright © 2019 Kukana. All rights reserved.
+//
+
+import XCTest
+@testable import FilmList
+
+class FilmListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
